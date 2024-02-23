fe4f8ed591e890e2d38644ae94b0338e0db49518
nanouuuuu <naellelouise@gmail.com>
2024-02-23 15:40:59 +0100

    js turn card ruby

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+$(".flip").click(function(){
+    $(this).parents(".card").toggleClass("flipped");
+  });
+  $(".clickcard").click(function(){
+    $(this).toggleClass("flipped");
+  });
